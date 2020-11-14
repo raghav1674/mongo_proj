73570bda973ba2aa658a807e3cea6dc38626c9ca
raghav <raghav.81-cse-17@mietjammu.in>
2020-11-14 14:24:15 +0530

    added: demo1.js and robotjs code

---
+++ code/01.js
@@ -0,0 +1,31 @@
+var i=10;
+
+console.log(i);
+
+var robot = require("robotjs");
+
+// // console.log(robot);
+
+// // robot.moveMouseSmooth(100,200);
+
+// // var position = robot.getMousePos();
+
+// // robot.moveMouseSmooth(40,12);
+
+// // robot.mouseClick("left");
+
+// // console.log(robot.getMousePos());
+// // console.log(position);
+
+// robot.moveMouse(98,840);
+// robot.mouseClick("left");
+// robot.setMouseDelay(2000);
+// robot.typeString(" chrome");
+// robot.keyTap("enter");
+// robot.setMouseDelay(5000);
+// robot.typeString("  https://www.youtube.com/results?search_query=CodeWithHarry");
+// robot.setMouseDelay(4000);
+// robot.keyTap("enter");
+robot.typeString("  Happy Diwali :)");
+
+
